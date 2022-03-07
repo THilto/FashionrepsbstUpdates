79b89032d054954c1b0f5e33560bb76258f62a6b
THilto <notnowted@gmail.com>
2022-03-07 17:30:10 +0000

    Changes to allow the use of &

--- bot.py
+++ bot.py
@@ -3,16 +3,24 @@ import time
 import requests
 import config
 
+# Reddit login data
 reddit_username = config.reddit['username']
 reddit_password = config.reddit['password']
 reddit_id = config.reddit['id']
 reddit_secret = config.reddit['secret']
 reddit_subreddit = config.reddit['subreddit']
 
+# Telegram data
 telegram_auth_token = config.telegram['auth_token']
 telegram_group_id = config.telegram['group_id']
 
 bot_launch_time = time.time()
+
+# Messages parameters
+message_params = {
+    "chat_id": telegram_group_id,
+    "text": "",
+}
 
 # Stream bot API
 reddit = praw.Reddit(client_id = reddit_id,
@@ -23,10 +31,12 @@ reddit = praw.Reddit(client_id = reddit_id,
 
 subreddit = reddit.subreddit(reddit_subreddit)
 
+# Telegram
 def send_message(message):
-    telegram_api_url = f"https://api.telegram.org/bot{telegram_auth_token}/sendMessage?chat_id=@{telegram_group_id}&text={message}"
-    tel_resp = requests.get(telegram_api_url)
+    message_params["text"] = message
+    requests.get(f"https://api.telegram.org/bot{telegram_auth_token}/sendMessage", params=params)
 
+# New posts stream
 for submission in subreddit.stream.submissions():
     if submission.created_utc > bot_launch_time:
         # Chat bot send message
